5c96c4e7bd1ae79cae42ffac65d05ec97f94be34
patricioarango <patricioarango@gmail.com>
2024-10-02 11:37:25 -0300

    Update proveedor_py.sql

--- proveedorPy/proveedor_py.sql
+++ proveedorPy/proveedor_py.sql
@@ -26,6 +26,37 @@ CREATE TABLE "talle" (
   PRIMARY KEY ("id")
 )
 
+
+CREATE TABLE `orden_de_compra` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `observaciones` TEXT DEFAULT NULL,
+  `id_store` int DEFAULT NULL,
+  `estado`  ENUM('RECHAZADA','ACEPTADA','SOLICITADA','RECIBIDA') DEFAULT 'SOLICITADA',
+  `fecha_creacion` DATETIME DEFAULT NULL,
+  `fecha` DATETIME DEFAULT NULL,
+  `fecha_recepcion` DATETIME DEFAULT NULL,
+  `id_despachon` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `orden_de_compra_item` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_orden_de_pago` int NOT NULL,
+  `codigo_producto` varchar(255) DEFAULT NULL,
+  `color` varchar(255) DEFAULT NULL,
+  `size` varchar(255) DEFAULT NULL,
+  `cantidad_solicitada` int DEFAULT NULL,
+   `procesado` int DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE `despacho` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_orden_de_pago` int NOT NULL,
+  `fecha_estimada_envio` DATETIME DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8; 
+
 insert into talle (talle) values ('chico');
 insert into talle (talle) values ('mediano');
 insert into talle (talle) values ('grande');
