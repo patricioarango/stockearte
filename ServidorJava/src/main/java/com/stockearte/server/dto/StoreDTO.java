2b60fb73c388c3699c15494b572bebb972d0879a
Martín Ottaviano <mmottaviano@gmail.com>
2024-10-07 00:02:40 -0300

    StoreCode en StoreDTO

--- ServidorJava/src/main/java/com/stockearte/server/dto/StoreDTO.java
+++ ServidorJava/src/main/java/com/stockearte/server/dto/StoreDTO.java
@@ -5,10 +5,12 @@ import com.stockearte.server.entities.Store;
 public class StoreDTO {
     private int idStore;
     private String storeName;
+    private String storeCode;
 
     public StoreDTO(Store store) {
         this.idStore = store.getIdStore();
         this.storeName = store.getStoreName(); 
+        this.storeCode = store.getStoreCode();
     }
 
     public int getIdStore() {
@@ -26,4 +28,13 @@ public class StoreDTO {
     public void setStoreName(String storeName) {
         this.storeName = storeName;
     }
+
+    public String getStoreCode() {
+        return storeCode;
+    }
+
+    public void setStoreCode(String storeCode) {
+        this.storeCode = storeCode;
+    }
+
 }
