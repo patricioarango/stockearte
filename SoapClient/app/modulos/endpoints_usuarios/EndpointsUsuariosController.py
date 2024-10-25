de590efd8a26a6a54666be51411229bb0a0126e2
patricioarango <patricioarango@gmail.com>
2024-10-24 21:12:34 -0300

    login de usuario

--- SoapClient/app/modulos/endpoints_usuarios/EndpointsUsuariosController.py
+++ SoapClient/app/modulos/endpoints_usuarios/EndpointsUsuariosController.py
@@ -2,7 +2,7 @@ from . import endpoints_usuarios_blueprint
 from flask import jsonify,request
 from flask import current_app as app
 from zeep import Client
-import os
+import os,requests
 
 @endpoints_usuarios_blueprint.route("/users", methods=["GET"])
 def endpoint_usuarios():
@@ -39,3 +39,44 @@ def add_user():
     
     response = client.service.addUser(user=new_user_data)
     return response
+
+@endpoints_usuarios_blueprint.route("/login", methods=["POST"])
+def login():
+    wsdl = os.getenv("SOAP_WSDL_USUARIOS")
+    client = Client(wsdl=wsdl)
+    data = request.get_json()
+    if data:
+        username = data.get('username')
+        password = data.get('password')
+        
+        # Check if username and password are provided
+        if not username or not password:
+            return {"error": "Username and password are required"}, 400
+        
+        username = data.get('username')
+        password = data.get('password')
+        response = client.service.userLogin(username=username, password=password)
+        print("response")
+        print(response)
+        return {response},200
+    return {"error": "No data provided"}, 400
+    
+@endpoints_usuarios_blueprint.route("/test_login", methods=["GET"])
+def test_login():
+    url = "http://localhost:5005/login"
+    data = {
+        "username": "johndoe1234",
+        "password": "securepassword"
+    }
+    try:
+        response = requests.post(url, json=data)
+        print(response)
+        
+        # Check if the response is successful
+        if response.status_code == 200:
+            return response.json(), response.status_code
+        else:
+            return {"error": "Login failed"}, response.status_code
+    except requests.exceptions.RequestException as e:
+        print(f"Error while sending POST request: {e}")
+        return {"error": "Request failed"}, 500
