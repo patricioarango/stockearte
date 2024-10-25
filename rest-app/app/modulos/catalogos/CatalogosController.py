7cb89703ff1b1a8cddaf17755435ffbdfdc9307f
gonzatarifa <gon17tarifa@gmail.com>
2024-10-24 22:37:06 -0300

    traigo productos y catalogos de tienda especifica

--- rest-app/app/modulos/catalogos/CatalogosController.py
+++ rest-app/app/modulos/catalogos/CatalogosController.py
@@ -1,7 +1,7 @@
 from . import catalogos_blueprint
 from flask import Flask, render_template, request, flash, redirect, session, json, url_for, redirect,jsonify
 import os,json
-from app.models import Catalog,CatalogProducts,Product
+from app.models import Catalog,CatalogProducts,Product,ProductStock
 from flask import current_app as app
 import datetime
 from app.models import db
@@ -14,26 +14,30 @@ def catalogos():
 
 @catalogos_blueprint.route('/catalogs', methods=['GET'])
 def get_catalogs():
-    # URL del endpoint de la API que devuelve los catálogos
-    url = 'http://localhost:5003/get_catalogs'
+    id_store = session.get('user_store_id')
+    print("ID de tienda obtenido de la sesión:", id_store)
+
+    if not id_store:
+        print("Error: No se encontró el ID de la tienda en la sesión.")
+        return render_template('catalog_list.html', catalogs=[])  
+
+    url = f'http://localhost:5003/get_catalogs?id_store={id_store}'
     response = requests.get(url)
 
     if response.status_code == 200:
-        catalogs = response.json()  # Obtener la lista de catálogos en formato JSON
-        # Renderizar el template HTML pasando la lista de catálogos
+        catalogs = response.json()  
         return render_template('catalog_list.html', catalogs=catalogs)
     else:
         print(f'Error al obtener catálogos: {response.status_code}')
-        return render_template('catalog_list.html', catalogs=[])  # En caso de error, pasa una lista vacía
+        return render_template('catalog_list.html', catalogs=[])  
 
  # Agregar un nuevo catálogo
 @catalogos_blueprint.route('/catalogs/new', methods=['GET', 'POST'])
 def add_catalog():
     if request.method == 'POST':
         catalog_name = request.form['catalog']
-        id_store = 1#request.form['id_store']
-
-        # Enviar solicitud POST al endpoint de la API
+        id_store = session.get('user_store_id')
+        print("contenido de la session:",session)
         url = 'http://localhost:5003/add_catalog'
         response = requests.post(url, json={'catalog': catalog_name, 'id_store': id_store})
 
@@ -48,7 +52,6 @@ def add_catalog():
 # Editar un catálogo
 @catalogos_blueprint.route('/catalogs/edit/<int:id>', methods=['GET', 'POST'])
 def edit_catalog(id):
-    # Obtener catálogo actual
     url = f'http://localhost:5003/get_catalogs'  # Endpoint para obtener los catálogos
     response = requests.get(url)
 
@@ -58,9 +61,8 @@ def edit_catalog(id):
 
         if request.method == 'POST':
             catalog_name = request.form['catalog']
-            id_store = 1#request.form['id_store']
+            id_store = session.get('user_store_id')
 
-            # Enviar solicitud PUT al endpoint de la API
             url = f'http://localhost:5003/edit_catalog/{id}'
             response = requests.put(url, json={'catalog': catalog_name, 'id_store': id_store})
 
@@ -78,7 +80,6 @@ def edit_catalog(id):
 # Eliminar un catálogo
 @catalogos_blueprint.route('/catalogs/delete/<int:id>', methods=['POST'])
 def delete_catalog(id):
-    # Enviar solicitud DELETE al endpoint de la API
     url = f'http://localhost:5003/delete_catalog/{id}'
     response = requests.delete(url)
 
@@ -96,43 +97,56 @@ def add_product_to_catalog(id_catalog):
         response = requests.post(url, json={'id_product': id_product})
 
         if response.status_code == 201:
-            # En vez de redirigir, recargar los productos asociados
             return redirect(url_for('catalogos.add_product_to_catalog', id_catalog=id_catalog))
         else:
-            print(f'Error al agregar producto: {response.status_code}, {response.text}')  # Agrega más detalles sobre el error
+            print(f'Error al agregar producto: {response.status_code}, {response.text}')  
             return redirect(url_for('catalogos.add_product_to_catalog', id_catalog=id_catalog))
 
-    # Obtener todos los productos disponibles
-    products = Product.query.all()  
+    catalog = Catalog.query.get(id_catalog)
+    if catalog:
+        id_store = catalog.id_store  
+        url = f'http://localhost:5003/stores/{id_store}/products'
+        response = requests.get(url)
 
-    # Obtener los productos asociados al catálogo
+        if response.status_code == 200:
+            products = response.json()  
+        else:
+            print(f'Error al obtener productos de la tienda: {response.status_code}, {response.text}')
+            products = []  
+    else:
+        products = []
+
     catalog_products = CatalogProducts.query.filter_by(id_catalog=id_catalog).all()
-
-    # Unir información del producto a partir de catalog_products
+    
     catalog_product_details = []
     for catalog_product in catalog_products:
-        product = Product.query.get(catalog_product.id_product)  # Obtener el producto por id
+        product = Product.query.get(catalog_product.id_product)  
         if product:
             catalog_product_details.append({
                 'catalog_product': catalog_product,
-                'product_name': product.product  # Suponiendo que `product` es el nombre del producto
+                'product_name': product.product  
             })
 
-    return render_template('add_product_to_catalog.html', id_catalog=id_catalog, products=products, catalog_product_details=catalog_product_details)
+    print("catalogos productos_detalle:", catalog_product_details)
+
+    return render_template('add_product_to_catalog.html', 
+                           id_catalog=id_catalog, 
+                           products=products, 
+                           catalog_product_details=catalog_product_details)
+
 
 @catalogos_blueprint.route('/catalogs/<int:id_catalog>/remove_product', methods=['POST'])
 def remove_product_from_catalog_view(id_catalog):
-    data = request.form  # Usa request.form si estás usando un formulario
+    data = request.form  
     id_product = data.get('id_product')
 
-    # Hacer la solicitud DELETE al endpoint de eliminación del producto
     url = f'http://localhost:5003/catalogs/{id_catalog}/remove_product'
-    response = requests.delete(url, json={'id_product': id_product})  # Cambiar a DELETE
+    response = requests.delete(url, json={'id_product': id_product})  
 
     if response.status_code == 200:
         return redirect(url_for('catalogos.add_product_to_catalog', id_catalog=id_catalog))
     else:
-        print(f'Error al eliminar producto: {response.status_code}, {response.text}')  # Para depuración
+        print(f'Error al eliminar producto: {response.status_code}, {response.text}') 
         return redirect(url_for('catalogos.add_product_to_catalog', id_catalog=id_catalog))
 
 
--- rest-app/app/modulos/endpoints_catalogos/EndpointsCatalogosController.py
+++ rest-app/app/modulos/endpoints_catalogos/EndpointsCatalogosController.py
@@ -4,30 +4,35 @@ import os,json
 
 from flask import current_app as app
 import datetime
-from app.models import db,Catalog,CatalogProducts,Product
+from app.models import db,Catalog,CatalogProducts,Product,ProductStock
 
 @endpoints_catalogos_blueprint.route("/endpoint_catalogos", methods=["GET"])
 def endpoint_catalogos():
     resp = jsonify(message="hello world")
     return resp
 
-# Endpoint para obtener todos los catálogos
 @endpoints_catalogos_blueprint.route('/get_catalogs', methods=['GET'])
 def get_catalogs():
-    catalogs = Catalog.query.all()
-    print(f'Catálogos encontrados: {len(catalogs)}')  # Debugging
+    id_store = request.args.get('id_store')
+    if id_store:
+        catalogs = Catalog.query.filter_by(id_store=id_store).all()
+    else:
+        catalogs = Catalog.query.all() 
+
+    print(f'Catálogos encontrados para id_store {id_store}: {len(catalogs)}') 
+
     return jsonify([{
         'id_catalog': catalog.id_catalog,
         'catalog': catalog.catalog,
         'id_store': catalog.id_store
     } for catalog in catalogs])
-
+    
 # Endpoint para agregar un nuevo catálogo
 @endpoints_catalogos_blueprint.route('/add_catalog', methods=['POST'])
 def add_catalog():
     data = request.json
     catalog_name = data.get('catalog')
-    id_store = 1#data.get('id_store')
+    id_store = data.get('id_store')
     
     new_catalog = Catalog(catalog=catalog_name, id_store=id_store)
     db.session.add(new_catalog)
@@ -46,26 +51,15 @@ def edit_catalog(id):
     db.session.commit()
     return jsonify({'message': 'Catálogo editado exitosamente'})
 
-# Endpoint para eliminar un catálogo
-@endpoints_catalogos_blueprint.route('/delete_catalog/<int:id>', methods=['DELETE'])
-def delete_catalog(id):
-    catalog = Catalog.query.get_or_404(id)
-    db.session.delete(catalog)
-    db.session.commit()
-    
-    return jsonify({'message': 'Catálogo eliminado exitosamente'})
-
 @endpoints_catalogos_blueprint.route('/catalogs/<int:id_catalog>/add_product', methods=['POST'])
-def add_product(id_catalog):  # Captura el id_catalog como argumento
+def add_product(id_catalog):  
     data = request.json
     id_product = data.get('id_product')
 
-    # Verificar si el producto ya existe en el catálogo
     existing_entry = CatalogProducts.query.filter_by(id_catalog=id_catalog, id_product=id_product).first()
     if existing_entry:
         return jsonify({'message': 'El producto ya está en el catálogo.'}), 400
     
-    # Crear la entrada del producto en el catálogo
     catalog_product = CatalogProducts(id_catalog=id_catalog, id_product=id_product)
     db.session.add(catalog_product)
     db.session.commit()
@@ -79,18 +73,39 @@ def get_products():
     products = Product.query.all()
     return jsonify([{'id_product': product.id_product, 'product_name': product.product_name} for product in products])
 
+@endpoints_catalogos_blueprint.route('/stores/<int:id_store>/products', methods=['GET'])
+def get_productos_por_tienda(id_store):
+    product_stocks = ProductStock.query.filter_by(id_store=id_store).all()
+
+    products = []
+    for stock in product_stocks:
+        product = Product.query.get(stock.id_product)  
+        if product:
+            products.append({
+                'idProduct': product.id_product,
+                'productName': product.product,
+                'code': product.code,
+                'color': product.color,
+                'size': product.size,
+                'img': product.img,
+                'stock': stock.stock  
+            })
+
+    if not products:
+        return jsonify({'message': 'No hay productos disponibles en esta tienda.'}), 404
+
+    return jsonify(products), 200
+
 # Endpoint para eliminar un producto del catálogo
 @endpoints_catalogos_blueprint.route('/catalogs/<int:id_catalog>/remove_product', methods=['DELETE'])
 def remove_product_from_catalog(id_catalog):
-    data = request.json  # Esperamos recibir datos en formato JSON
+    data = request.json  
     id_product = data.get('id_product')
-
-    # Buscar la entrada del producto en el catálogo
+    
     catalog_product = CatalogProducts.query.filter_by(id_catalog=id_catalog, id_product=id_product).first()
     if not catalog_product:
         return jsonify({'message': 'El producto no está en el catálogo.'}), 404
-
-    # Eliminar la entrada del producto en el catálogo
+    
     db.session.delete(catalog_product)
     db.session.commit()
 
